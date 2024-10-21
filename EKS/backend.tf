147d58fad4e8098ed4689169ed82ffa8c41372f2
anu8002 <anu8002cta@gmail.com>
2024-10-21 20:17:29 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ace-project-bucket"  # Replace with your actual S3 bucket name
+    bucket = "project-bucket-ace"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "us-east-1" 
+    region = "ap-south-1" 
   }
 }
